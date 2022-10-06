27eab1e9151499c35e89687c9be076d3d7e100f9
Ben Salha Mohamed Amine <mohamed-amine.ben-salha@student-cs.fr>
2022-10-06 09:13:27 +0100

    reorganize

--- apps/main_streamlit.py
+++ apps/main_streamlit.py
@@ -4,7 +4,7 @@ from utils_streamlit import *
 device = load_device()
 model = import_model(bucket="mbenxsalha", key="diffusion/state_dict.pickle", device=device)
 
-st.title("Welcome to The Ship Detective!")
+st.title("Welcome to The Pizza Detective!")
 st.write("The image you upload will be fed to a Deep Neural Network in real-time to verify if there is a ship or not")
 file = st.file_uploader("Upload an image")
 
